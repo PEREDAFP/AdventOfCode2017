e484d2ba5206de8752c79025dcdffeb7c1a9d45c
PEREDAFP <PEDROPEREDA@HOTMAIL.COM>
2021-08-31 20:10:22 +0200

    Create dia4.rs

---
+++ dia4/dia4.rs
@@ -0,0 +1,22 @@
+mod lib_advance_of_code4;
+use lib_advance_of_code4::Config;
+const USAGE: &str  = "Debes utilizar <ejecutable> <diax> <diax.txt>";
+
+fn main(){
+    let config = match Config::new(std::env::args()){
+        Ok(c)  => c,
+        Err(e) => {
+            println!("Se ha producido el error: {}", e);
+            println!("{}", USAGE);
+            std::process::exit(1);
+        }
+    };
+
+    match config.get_day() {
+        "4" =>  {
+                println!("El resultado de día 2 parte 1 es: {}",config.run_dia4());
+                println!("El resultado de día 2 parte 2 es: {}",config.run_dia4_2())
+            },
+        _ => println!("No has elegido un día correcto"),
+    };
+}
